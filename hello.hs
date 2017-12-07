c77094c33fd7b7471bc1945db21412abb4bdce86
Zepp Lu <luzepu678@gmail.com>
2017-12-07 18:57:56 +0800

    gitignore added

---
+++ hello.hs
@@ -0,0 +1,12 @@
+-- What does `return' mean? It's tow main purposes?
+
+main = do   
+    line <- getLine  
+    if null line  
+        then return ()  
+        else do  
+            putStrLn $ reverseWords line  
+            main  
+  
+reverseWords :: String -> String  
+reverseWords = unwords . map reverse . words  
